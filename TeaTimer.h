95282bdc13d93603ca6282642e475a35c16f1e11
Madison Bennett <114691834+madBennett@users.noreply.github.com>
2024-09-30 16:17:00 -0700

    Initial Commit
    -Add source files and base structure
    -PLEASE BE SURE TO ADD YOUR GUYS NAMES AND RED ID TO THE FILES HEADER

---
+++ TeaTimer.h
@@ -0,0 +1,11 @@
+/* TeaTimer.h CS530 SDSU
+main source File
+
+Madison Bennett
+id: 824800113
+
+... add yours
+
+
+
+*/
---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* main.cpp CS530 SDSU
+main source File
+
+Madison Bennett
+id: 824800113
+
+... addd yours
+
+
+
+*/
+
+#include "TeaTimer.h"
